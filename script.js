b63085206078cdee5760aad49b31065b68ee74fc
parentClass <dimalantadaniel@gmail.com>
2024-07-10 01:01:33 +0800

    added datatable, honeypot pass

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+$(document).ready( function () {
+    $('#rsvp-table').DataTable({
+        paging: true,
+        ordering: true,
+        searching: true,
+        lengthChange: false,
+        columns: [
+            null,
+            { width: '50%' },              
+            null,
+            { width: '30%' },
+            null,
+            null,
+            null
+        ]
+    });
+} );
+
 function loadRsvps() {
     $.ajax({
         type: "GET",
@@ -5,45 +23,61 @@ function loadRsvps() {
         success: function(response) {
             j = JSON.parse(response);
 
-            j.forEach(rsvp => {
-                $("#rsvp-table-body").append(
-                    `
-                        <tr>
-                            <th scope="row">${rsvp.id}</th>
-                            <td>${rsvp.name}</td>
-                            <td>${rsvp.mobile}</td>
-                            <td>${rsvp.email}</td>
-                            <td>${rsvp.message}</td>
-                            <td class="${rsvp.is_pasilungan_attending == 1 ? "table-success" : "table-danger"}">
-                                ${rsvp.is_pasilungan_attending == "1" ? "Yes" : "No"}
-                            </td>
-                            <td class="${rsvp.is_approved == 1 ? "table-success" : "table-danger"}">
-                                ${rsvp.is_approved == "1" ? "Yes" : "Not yet"}
-                            </td>
-                            <td ${rsvp.is_approved != 1 ? '' : 'style="display: none;"'}>
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                    <button type="button" class="btn btn-success" onclick="approve('${rsvp.id}', '${rsvp.email}')">Approve</button>
-                                    <button type="button" class="btn btn-danger">Decline</button>
-                                </div>
-                            </td>
-                        </tr>
-                    `
-                )
+            var table = $('#rsvp-table').DataTable();
+            table.clear();
+
+            j.forEach(function(rsvp) {
+                var isApprovedCell = rsvp.is_approved == 1 ? "Yes" : "Not yet";
+                var isAttendingCell = rsvp.is_pasilungan_attending == 1 ? "Yes" : "No";
+
+                var approveAndDeclineButtonHtml = '';
+
+                if (rsvp.is_approved != 1) {
+                    approveAndDeclineButtonHtml = '<button type="button" class="btn btn-success" onclick="approve(' + rsvp.id + ', \'' + rsvp.email + '\')">Approve</button>' + '<button type="button" class="btn btn-danger">Decline</button>';
+                }
+        
+                table.row.add([
+                    rsvp.id,
+                    rsvp.name,
+                    rsvp.mobile,
+                    rsvp.email,
+                    isAttendingCell,
+                    isApprovedCell,
+                    '<div class="btn-group" role="group" aria-label="Basic example">' +
+                        '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#rsvp-message-modal" onclick="viewMessage(\'' + rsvp.name + '\', \'' + addslashes(rsvp.message) + '\')">View</button>' +
+                        approveAndDeclineButtonHtml +
+                    '</div>'
+                ]).draw(false);
             });
         }
     });
 }
 
+function viewMessage(name, message) {
+    $("#message-modal-title").html(name + " said...")
+    $("#message-modal-body").html(message);
+}
+
 function approve(id, email) {
     $.ajax({
         type: "POST",
-        url: "http://localhost:8080/rsvp/approve",
+        url: "https://api.dsciwedding.com/rsvp/approve",
         data: JSON.stringify({'id': id, 'email': email}),
         success: function (response) {
             console.log(response);
             j = JSON.parse(response);
         }
     })
+}
+
+function addslashes( str ) {
+    return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
+
+var password = prompt("");
+
+if (btoa(password) != "SmF4eGRtczIwQA==") {
+    location = "honeypot.html"
 }
 
 loadRsvps();
